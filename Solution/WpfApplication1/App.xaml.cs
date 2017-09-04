4da7840ce94e2c8a9e591bedd77cde36a0e85a1b
Oscar Jimenez <social@ittxa.com>
2017-09-04 15:49:28 +0200

    no message

--- Solution/WpfApplication1/App.xaml.cs
+++ Solution/WpfApplication1/App.xaml.cs
@@ -5,7 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
-using NetStandard20;
+using NetStandard16;
 using System.Diagnostics;
 
 namespace WpfApplication1
