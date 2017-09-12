0b6119b5445ed41fc9e461a5aaf821704b156e29
Oscar Jimenez <social@ittxa.com>
2017-09-12 14:03:49 +0200

    vale

--- Solution/WpfApp1/App.xaml.cs
+++ Solution/WpfApp1/App.xaml.cs
@@ -20,6 +20,7 @@ namespace WpfApp1
             Class1 c1 = new Class1();
             var tup = c1.GetTuple();
             // VAmos . oo - kk - 1 - 2 - 3
+            // F2 => 1 - 2 - 3
             Debug.WriteLine("" + tup.First);
         }
     }
