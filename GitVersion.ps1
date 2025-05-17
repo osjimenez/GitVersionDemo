a2b2f9316d486985996d40018f8a99ce7f93e5db
Oscar Jimenez <oscar@lyberum.com>
2025-05-17 15:08:17 +0200

    -

--- GitVersion.ps1
+++ GitVersion.ps1
@@ -9,6 +9,7 @@ $ymlFiles = Get-ChildItem -Path $folder -Filter "*.yml" | Sort-Object { [System.
 
 foreach ($file in $ymlFiles) {
     $nameWithoutExtension = [System.IO.Path]::GetFileNameWithoutExtension($file.Name)
-    $version = & $gitVersionExe /showvariable FullSemVer /config $file.FullName
-    Write-Host "$nameWithoutExtension = $version"
+    $fullSemVer = & $gitVersionExe /showvariable FullSemVer /config $file.FullName
+    $InformationalVersion = & $gitVersionExe /showvariable InformationalVersion /config $file.FullName
+    Write-Host "$nameWithoutExtension = $fullSemVer   ($InformationalVersion)"
 }
