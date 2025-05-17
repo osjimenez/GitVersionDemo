c1f9ae25eedc4d558a806f90258e99b094cc138f
Oscar Jimenez <oscar@lyberum.com>
2025-05-17 14:37:09 +0200

    -

--- GitVersion.ps1
+++ GitVersion.ps1
@@ -4,14 +4,11 @@ $gitVersionExe = ".\GitVersion.exe"
 # Carpeta donde están los archivos .yml
 $folder = "."
 
-# Buscar todos los archivos .yml en la carpeta
-$ymlFiles = Get-ChildItem -Path $folder -Filter "*.yml"
+# Buscar, ordenar alfabéticamente por nombre (sin extensión)
+$ymlFiles = Get-ChildItem -Path $folder -Filter "*.yml" | Sort-Object { [System.IO.Path]::GetFileNameWithoutExtension($_.Name) }
 
 foreach ($file in $ymlFiles) {
     $nameWithoutExtension = [System.IO.Path]::GetFileNameWithoutExtension($file.Name)
-    
-    # Ejecutar GitVersion con ese archivo
     $version = & $gitVersionExe /showvariable FullSemVer /config $file.FullName
-
-    Write-Host "$nameWithoutExtension = $version"
+    Write-Host "$nameWithoutExtension ? $version"
 }
